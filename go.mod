8ebbad7a69edd961cb0a3063144d62d4a50d0d8f
danielattilavanyi <152228710+danielattilavanyi@users.noreply.github.com>
2023-11-27 23:53:27 +0100

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module projects/calendar
+
+go 1.21.4
---
+++ setup.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	MaxRetries = 3
+	Timeout    = 10
+)
+
+var months = [12]string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "Oktober", "November", "December"}
